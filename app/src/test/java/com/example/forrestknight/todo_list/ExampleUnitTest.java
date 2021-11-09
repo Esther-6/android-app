fadef392319e35678d274e86e4686904d10f3dab
xy的电脑 <xydediannao@xydediannaodeMacBook-Pro.local>
2021-11-09 14:37:29 +0800

    third commit

--- app/src/test/java/com/example/forrestknight/todo_list/ExampleUnitTest.java
+++ app/src/test/java/com/example/forrestknight/todo_list/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.forrestknight.todo_list;
 
 import org.junit.Test;
 
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
 }
